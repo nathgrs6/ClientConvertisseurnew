84e2b65f2335183692ea97c5056a6ded8b093f5b
nathgrs6 <nathan.grosjean@etu.univ-smb.fr>
2023-02-01 17:01:38 +0100

    ca marche bien

---
+++ ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
@@ -0,0 +1,80 @@
+﻿using ClientConvertisseurV2.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSConvertisseur.Models;
+
+namespace ClientConvertisseurV2.ViewModels
+
+{
+    public class ConvertisseurEuroViewModel : ObservableObject
+    {
+        public ConvertisseurEuroViewModel()
+        {
+            GetDataOnLoadAsync();
+
+        }
+        private ObservableCollection<Devise> lesDevises;
+
+        public ObservableCollection<Devise> LesDevises
+        {
+            get { return lesDevises; }
+            set { lesDevises = value; OnPropertyChanged(); }
+        }
+
+        private Devise selectedDevise;
+
+
+        public Devise SelectedDevise
+        {
+            get { return selectedDevise; }
+            set { selectedDevise = value; OnPropertyChanged(); }
+        }
+
+        private double montant;
+
+        public double Montant
+        {
+            get { return montant; }
+            set { montant = value; OnPropertyChanged(); }
+        }
+
+        private double convertedAmount;
+
+        public double ConvertedAmount
+        {
+            get { return convertedAmount; }
+            set { convertedAmount = value; OnPropertyChanged(); }
+        }
+
+
+
+
+ 
+
+
+
+        public async void GetDataOnLoadAsync()
+        {
+            WSService service = new WSService("https://localhost:7008/api/");
+            List<Devise> result = await service.GetDevisesAsync("devises");
+            if (result == null)
+            {
+
+                return;
+
+            }
+            else
+            {
+                LesDevises = new ObservableCollection<Devise>(result);
+            }
+
+        }
+    }
+    
+}
